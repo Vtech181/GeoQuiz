7d27a00aaf64203bfdeb692d5e9bc6ab456b70de
VAIBHAV CHAUHAN <vaibhavchauhan94.vc@gmail.com>
2017-01-14 22:02:04 +0530

    Completed till page 128 with confusion in mAnswerKey and mQuestionBank

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,6 +18,7 @@ public class QuizActivity extends Activity {
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
+	private Button mCheatButton;
 	private TextView mQuestionTextView;
 	private int mCurrentIndex = 0;
 
@@ -79,6 +81,19 @@ public class QuizActivity extends Activity {
 		if (savedInstanceState != null) {
 			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
 		}
+		mCheatButton = (Button) findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+				startActivity(i);
+			}
+		});
+
 		updateQuestion();
 	}
 
